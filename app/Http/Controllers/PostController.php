c7e6ba2426421a909c3509ea3c977185b5df6538
osramywj <1027612662@qq.com>
2017-08-08 17:37:33 +0800

    0808

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,17 +10,29 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('post.index');
+        $posts =[
+            ['title'=>1111,'content'=>11111111111111111111111],
+            ['title'=>222,'content'=>11111111111111111111111],
+            ['title'=>333,'content'=>11111111111111111111111],
+            ['title'=>4444,'content'=>11111111111111111111111],
+            ['title'=>5555,'content'=>11111111111111111111111],
+            ['title'=>6666,'content'=>11111111111111111111111],
+            ['title'=>777,'content'=>11111111111111111111111],
+            ['title'=>8888,'content'=>11111111111111111111111],
+            ['title'=>9999,'content'=>11111111111111111111111],
+            ['title'=>456456,'content'=>11111111111111111111111],
+        ];
+        return view('post.index',compact('posts'));
     }
 
     public function show()
     {
-        return view('post.show');
+        return view('post.show',['title'=>'this is a title','isShow'=>false]);
     }
 
     public function create()
     {
-        
+        return view('post.create');
     }
 
     public function store()
@@ -30,7 +42,7 @@ class PostController extends Controller
 
     public function edit()
     {
-
+        return view('post.edit');
     }
 
     public function update()
